0ce1fb833f8135b068f981ab4386a57390c06e77
lark1987 <bosworth210920@gmail.com>
2023-10-17 11:56:49 +0800

    後端資料查詢

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import *
 
+from api.today_weather import api_today_weather
+from api.week_weather import api_week_weather
+
 app = Flask(__name__)
+
+app.register_blueprint(api_today_weather)
+app.register_blueprint(api_week_weather)
 
 # 請勿更動此主路由
 @app.route("/")
